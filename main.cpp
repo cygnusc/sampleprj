cf9dc1123b96eba1caa53375c378918688f4493b
Ru Zhu <rogxeon@gmail.com>
2015-05-03 19:19:23 -0500

    Collision detection
    - move around the large rect

--- main.cpp
+++ main.cpp
@@ -22,35 +22,6 @@ int main()
 	//Event object holding all events
 	sf::Event event;
 
-	//States for button/events
-	bool aPressed = false;
-	bool aReleased = false;
-	bool space = false;
-	bool leftClick = false;
-
-	//Variables
-	int numberOfClicks = 0;	//For counting the number of clicks
-	int mouseX = 0, mouseY = 0;		//For storing the mouse position
-	int rectXPosition = 0;	//Rectangles X position
-
-	//Images
-	sf::Texture image1; //Image/texture object
-	
-
-	//Render shapes
-	//Rectangle shape
-	sf::RectangleShape rect;
-	rect.setSize(sf::Vector2f(100, 100)); //Width and height
-	rect.setPosition(0, 0); //Position
-	rect.setFillColor(sf::Color::White); //White fill color so we don't change the images colors
-	//rect.setTexture(&image1); //Bind an image/texture to the rectangle
-
-	//Circle shape
-	sf::CircleShape circle;
-	circle.setRadius(50); //Radius
-	circle.setPosition(400, 300); //Position
-	circle.setFillColor(sf::Color::Blue); //Color
-
 	// font
 	sf::Font font;
 	if (font.loadFromFile("Data/arial.ttf") == 0)
@@ -58,36 +29,27 @@ int main()
 		return 1;
 	}
 
-	// text
-	sf::Text title;
-	title.setFont(font);
-	title.setCharacterSize(30);
-	title.setString("Hello World!");
-	title.setPosition(300, 50);
-	title.setColor(sf::Color::Magenta);
+	// States
+	bool rButton = false;
+	bool leftButton = false;
+	bool rightButton = false;
+	bool upButton = false;
+	bool downButton = false;
 
-	// Sounds
-	sf::SoundBuffer ExplosionBuffer;
-	if (ExplosionBuffer.loadFromFile("Data/explosion.wav") == 0)
-	{
-		return 1;
-	}
+	// Variables
+	int rectRotation = 0;
+	int xVelocity = 0;
+	int yVelocity = 0;
 
-	sf::Sound explosion;
-	explosion.setBuffer(ExplosionBuffer);
-	explosion.setVolume(200);
-	//explosion.setLoop(true);
-	explosion.play();
+	// Shapes
+	sf::RectangleShape shape1;
+	shape1.setSize(sf::Vector2f(50, 50));
+	shape1.setPosition(400, 300);
 
-	// Music
-	sf::Music anthem;
-	if (anthem.openFromFile("Data/anthem.ogg") == 0)
-	{
-		return 1;
-	}
-	anthem.setVolume(200);
-	anthem.play();
-	
+	sf::RectangleShape shape2;
+	shape2.setSize(sf::Vector2f(100, 200));
+	shape2.setPosition(200, 200);
+	shape2.setFillColor(sf::Color::Red);
 
 	//Game loop
 	while (play == true)
@@ -95,91 +57,106 @@ int main()
 		//EVENTS
 		while (window.pollEvent(event))
 		{
-			//Event type is key pressed and the key is 'A'
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
-			{
-				//Set the state to true
-				aPressed = true;
-			}
-
-			//Event type is key released and the key is 'A'
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::A)
-			{
-				aReleased = true;
-			}
-
-			//Space key pressed
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-			{
-				space = true;
-			}
-
-			//Space key released
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Space)
-			{
-				space = false;
-			}
-
-			//Left mouse button pressed down (clicked)
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-			{
-				leftClick = true;
-			}
-
-			//Mouse moved in the window
-			if (event.type == sf::Event::MouseMoved)
-			{
-				mouseX = event.mouseMove.x; //X position of the mouse
-				mouseY = event.mouseMove.y; //Y position of the mouse
-			}
-
 			//Event type is window closed
 			if (event.type == sf::Event::Closed)
 			{
 				//Set play to false in order to stop the game loop
 				play = false;
 			}
+
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
+			{
+				rButton = true;
+			}
+
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::R)
+			{
+				rButton = false;
+			}
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Left) leftButton = true;
+				if (event.key.code == sf::Keyboard::Right) rightButton = true;
+				if (event.key.code == sf::Keyboard::Up) upButton = true;
+				if (event.key.code == sf::Keyboard::Down) downButton = true;
+			}
+
+			if (event.type == sf::Event::KeyReleased)
+			{
+				if (event.key.code == sf::Keyboard::Left) leftButton = false;
+				if (event.key.code == sf::Keyboard::Right) rightButton = false;
+				if (event.key.code == sf::Keyboard::Up) upButton = false;
+				if (event.key.code == sf::Keyboard::Down) downButton = false;
+			}
 		}
 
-		//LOGIC
-		if (aPressed == true)
+		// Logic
+		if (rButton == true)
 		{
-			//Print to the console
-			std::cout << "The A key has been pressed\n";
-
-			aPressed = false;
+			rectRotation++;
+			shape1.setRotation(rectRotation);
 		}
 
-		if (aReleased == true)
+		// Movement
+		if (rightButton == true)
 		{
-			std::cout << "The A key has been released\n";
-
-			aReleased = false;
+			xVelocity = 5;
 		}
-
-		//If the left mouse button was clicked
-		if (leftClick == true)
+		if (leftButton == true)
 		{
-			numberOfClicks++; //numberOfClicks = numberOfClicks + 1;
-
-			std::cout << "Number of clicks is " << numberOfClicks << "\n";
-
-			leftClick = false;
+			xVelocity = -5;
 		}
 
-		//Print out the position of the mouse.
-		std::cout << "Mouse x: " << mouseX << " mouse y: " << mouseY << "\n";
+		if (leftButton == true && rightButton == true)
+		{
+			xVelocity = 0;
+		}
 
-		//Basic movement
-		rectXPosition++; //X position variable of the rectangle
-		rect.setPosition(rectXPosition, rectXPosition); //We set the rectangles X and Y position to the variable rectXPosition
+		if (leftButton == false && rightButton == false)
+		{
+			xVelocity = 0;
+		}
 
+		if (upButton == true)
+		{
+			yVelocity = -5;
+		}
+		if (downButton == true)
+		{
+			yVelocity = 5;
+		}
+
+		if (upButton == true && downButton == true)
+		{
+			yVelocity = 0;
+		}
+
+		if (upButton == false && downButton == false)
+		{
+			yVelocity = 0;
+		}
+
+		// Move
+		shape1.move(xVelocity, 0);
+
+		if (shape1.getGlobalBounds().intersects(shape2.getGlobalBounds()) == true)
+		{
+			shape1.move(-xVelocity, 0);
+		}
+
+		shape1.move(0, yVelocity);
+
+		if (shape1.getGlobalBounds().intersects(shape2.getGlobalBounds()) == true)
+		{
+			shape1.move(0, -yVelocity);
+		}
+		
 		//RENDERING
 		window.clear();
 
-		window.draw(rect); //Draw the rectangle shape
-		window.draw(circle); //Draw the circle shape
-		window.draw(title);
+		window.draw(shape1);
+		window.draw(shape2);
 
 		window.display();
 	}
